2b66849fa7499ea8fa1dd6c7bcfc2ebd59b5c9a3
jimt <jamesrobtait@gmail.com>
2024-12-01 00:41:12 +0000

    setup

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A flake for Advent of Code 2024";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    overlays = [];
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import nixpkgs {inherit overlays system;};
+        });
+  in {
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          just gleam erlang
+        ];
+      };
+    });
+  };
+}
