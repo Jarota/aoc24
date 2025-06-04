dafe5543dadc273290a7c8c15291b9b6d3909385
jimt <jamesrobtait@gmail.com>
2025-06-04 23:35:43 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -9,18 +9,17 @@
     self,
     nixpkgs,
   }: let
-    overlays = [];
     supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
     forEachSupportedSystem = f:
       nixpkgs.lib.genAttrs supportedSystems (system:
         f {
-          pkgs = import nixpkgs {inherit overlays system;};
+          pkgs = import nixpkgs {inherit system;};
         });
   in {
     devShells = forEachSupportedSystem ({pkgs}: {
       default = pkgs.mkShell {
         packages = with pkgs; [
-          just gleam erlang_27
+          just gleam erlang_28
         ];
       };
     });
